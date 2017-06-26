df5a72bd9672a5ec382fa761fc5db706690212fb
David <david.hyman@tesco.net>
2017-06-26 19:26:34 +0100

    pypi bits

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('requirements.txt') as fh:
+    requirements = fh.readlines()
+
+setup(
+    name='PyOverride',
+    packages=['override'],
+    version='0.9.0',
+    license='MIT',
+    description='Python project configuration',
+    author='David Hyman',
+    author_email='david.hyman...@gmail.com',
+    url='https://github.com/davidhyman/override',
+    download_url='https://github.com/davidhyman/override/archive/0.9.tar.gz',
+    keywords=[
+        'settings',
+        'configuration',
+        'override',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3',
+    install_requires=requirements,
+)
